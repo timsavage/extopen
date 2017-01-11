ccf1263edf91b656f33e79557d3f00f510fa6803
Tim Savage <tim@savage.company>
2017-01-12 10:36:19 +1100

    Bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'extopen',
-    version = '0.1',
+    version = '0.1.1',
     description = "Cross platform helper for opening a file with the default external application.",
     long_description = open('README.rst').read(),
     url='https://github.com/timsavage/extopen',
